e813d16c5949f3322a067946b9bd2a077d72b71d
Abhishek13 <abhishek7109501@gmail.com>
2024-01-13 20:20:22 +0530

    c

--- src/component/Testimonial.jsx
+++ src/component/Testimonial.jsx
@@ -37,7 +37,7 @@ const Testimonial = () => {
       const width = window.innerWidth;
       let newSlidesToShow;
 
-      if (width < 430) {
+      if (width < 480) {
         newSlidesToShow = 1;
       } else if (width < 960) {
         newSlidesToShow = 2;
